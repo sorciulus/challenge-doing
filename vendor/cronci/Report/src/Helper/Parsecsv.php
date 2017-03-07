be5caecf6b43cb85fd22f47c1062827ea5fda657
CorradoRonci <corrado.ronci@servicein.it>
2017-03-07 13:20:59 +0100

    Remove misprints

--- vendor/cronci/Report/src/Helper/Parsecsv.php
+++ vendor/cronci/Report/src/Helper/Parsecsv.php
@@ -1,27 +1,36 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of parseCsv
+ * Classe per legge file da un file csv
  *
  * @author Corrado
  */
 namespace Report\Helper;
 
-class Parsecsv {
+class Parsecsv implements \Report\Model\ModelInterface {
     
+    /**
+     * @var integer[]
+     */
     private $result;
+    /**
+     * @var string
+     */
+    private $file;
     
+    /**
+    *  Se la costante ABS non è definita o il file non esiste
+    *  il metodo ritorna una Exception 
+    * 
+    * @throws \Exception
+    */
     public function __construct() {
         
         if(defined("ABS")){
-            if(file_exists(ABS.DIRECTORY_SEPARATOR."data.csv") ){
-                $this->parse();                      
+            $file = ABS.DIRECTORY_SEPARATOR."data.csv";            
+            if(file_exists($file) ){
+                $this->file = $file;
+                $this->parse();
             }else{
                 throw new \Exception('File data.csv non trovato in '.ABS);
             }
@@ -31,14 +40,28 @@ class Parsecsv {
         
     }
     
+    /**
+     * Metodo per la lettura e il salvataggio del file csv
+     *
+     * Questo metodo legge il file e lo memorizza nella proprietà $result 
+     * 
+     * Se il file non può essere letto il metodo restituisce una Exception
+     *
+     *
+     * @return void
+     * @throws \Exception
+     */
     private function parse(){
-        
-        $resource = fopen($filename, 'r');
-        $headers = fgetcsv($resource, 1024, ';');       
+                
+        $resource = fopen($this->file, 'r') or die("Impossibile aprire il file $this->file");
+        $headers = fgetcsv($resource, 0, ';');       
 
         $data = [];
         while (!feof($resource)){
-            $row_temp = fgetcsv($resource, 1024, ';');
+            $row_temp = fgetcsv($resource, 0, ';');
+            if(!$row_temp){
+                continue;
+            }
             foreach ($headers as $key => $header){
                 if(isset($row_temp[$key])){
                     $row[$header] = $row_temp[$key];
@@ -46,16 +69,34 @@ class Parsecsv {
             }
             array_push($data, $row);
         }
-        fclose($resource);
+        fclose($resource);        
         $this->result = $data;
         
     }
     
-    function getResult() {
-        return $this->result;
-    }
-
-    function getResultObject(){
-        return (object) $this->result;
-    }
+    
+    /**
+     * Metodo per la ricerca del valore 'customer'
+     *
+     * Questo metodo ricerca il valore nella proprietà $result restituendo un array
+     * 
+     * Se la ricerca non produce risultati il metodo restituisce un array vuoto
+     *
+     * @param integer $customerId
+     * @return array
+     */
+    function getResultFromCustomerId($customerId){
+        
+        $data = [];
+        if($this->result){            
+            foreach ($this->result as $row){
+                if($row["customer"] == $customerId){
+                    $data[] = $row;
+                }
+            }
+        }
+        return $data;
+    }        
+    
+    
 }
